5b6051356d12b5f9b0142626821b3797ae86716b
MariiaSam <128381107+MariiaSam@users.noreply.github.com>
2023-08-18 08:45:28 +0000

    Deploying to gh-pages from @ MariiaSam/goit-js-hw-09@629deff83cc45bc149cecb1ea16fd07121a4307a 🚀

---
+++ 01-color-switcher.e4b1c326.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,t=setInterval((function(){d.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),a.addEventListener("click",(function(){e.disabled=!1,a.disabled=!0,clearInterval(t)})),a.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.e4b1c326.js.map
