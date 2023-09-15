9e0960cdcfb0c44ad3096153a1f5ead668f0ca62
MariiaSam <kontragent@ukr.net>
2023-09-15 16:11:49 +0300

    add 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const elements = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+
+elements.form.addEventListener('click', handlePromiseCreate);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handlePromiseCreate(evt) {
+  evt.preventDefault();
+
+  let valueDelay = Number(elements.delay.value);
+  let step = Number(elements.step.value);
+  let amount = Number(elements.amount.value);
+
+  for (let i = 1; i <= amount; i+=1) {
+    let promiseDelay = valueDelay + step * i;
+
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
